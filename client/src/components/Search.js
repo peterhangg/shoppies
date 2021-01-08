262877b5e346077ff7798d69569ea7bf5d57f65c
peter <peterhanggg@gmail.com>
2021-01-08 16:51:51 -0500

    redux action, reducer for fetching movies and moved form and movies into their own component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { gql, useLazyQuery } from "@apollo/client";
+import { useDispatch } from 'react-redux';
 import useDebounce from "../hook/useDebounce";
+import { getMovies } from "../actions/getMovies";
 
 const GET_MOVIES = gql`
   query GetMovies($title: String!) {
@@ -16,8 +18,15 @@ const GET_MOVIES = gql`
 const Search = () => {
   const [title, setTitle] = useState("");
   const searchValue = useDebounce(title, 700);
+  const dispatch = useDispatch();
 
-  const [loadMovies, { data, loading, error }] = useLazyQuery(GET_MOVIES);
+  const [loadMovies, moviedata] = useLazyQuery(GET_MOVIES);
+
+  useEffect(() => {
+      if (moviedata) {
+        dispatch(getMovies(moviedata));
+      }
+  },[dispatch, moviedata]);
 
   const updateSearch = (e) => {
     setTitle(e.target.value);
