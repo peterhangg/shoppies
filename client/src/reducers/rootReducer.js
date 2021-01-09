f13d368d9c479ecbf10f833ca00b3c3de18e06a1
peter <peterhanggg@gmail.com>
2021-01-09 16:57:49 -0500

    added functionality to add and delete movies for moninations, and nominations component

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import getMoviesReducer from "./getMoviesReducer";
+import nominationsReducer from "./nominationsReducer";
+import getSearchReducer from "./getSearchReducer";
 
 const rootReducer = combineReducers({
   getMoviesReducer,
+  nominationsReducer,
+  getSearchReducer
 });
 
 export default rootReducer;
