48f7095b64f5a09840ff347a8c403b112b6a012a
peter <peterhanggg@gmail.com>
2021-01-11 16:53:15 -0500

    styled movie grid and movie list

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -3,37 +3,45 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { useDispatch } from "react-redux";
 import { addNomination } from "../actions/nominationActions";
+import { MovieGrid, MovieStyles } from "../styles/index";
+import PosterPlaceholder from "../asset/movie-placeholder.jpeg";
 const Movies = ({ data, loading, error, nominations, search }) => {
   const dispatch = useDispatch();
 
   return (
-    <>
+    <div>
       {loading && <p>Loading...</p>}
       {error && <p>Oops there was an error. Please search for another movie</p>}
+      <h1>MOVIES</h1>
+      {search && <h3>RESULTS FOR: {search}</h3>}
       {data && data.movies.length > 0 && (
-        <div>
-          <h1>MOVIES</h1>
-          {search && <h3>Results for {search}</h3>}
-          <ul>
-            {data.movies.map((movie) => (
-              <li key={movie.id}>
-                <p>{movie.title}</p>
-                <p>{movie.year}</p>
-                <button
-                  type="button"
-                  disabled={
-                    nominations.includes(movie) || nominations.length === 5
-                  }
-                  onClick={() => dispatch(addNomination(movie))}
-                >
-                  Nominate
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <MovieGrid overflow={"scroll"}>
+          {data.movies.map((movie) => (
+            <MovieStyles key={movie.id}>
+              <a
+                href={`https://www.imdb.com/title/${movie.id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={movie.poster !== "N/A" ? movie.poster : PosterPlaceholder} alt={movie.name} />
+              </a>
+              <p>
+                {movie.title} ({movie.year})
+              </p>
+              <button
+                type="button"
+                disabled={
+                  nominations.includes(movie) || nominations.length === 5
+                }
+                onClick={() => dispatch(addNomination(movie))}
+              >
+                Nominate
+              </button>
+            </MovieStyles>
+          ))}
+        </MovieGrid>
       )}
-    </>
+    </div>
   );
 };
 
--- client/src/components/Nominations.js
+++ client/src/components/Nominations.js
@@ -3,32 +3,38 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { useDispatch } from "react-redux";
 import { deleteNomination } from "../actions/nominationActions";
+import { MovieGrid, MovieStyles } from "../styles/index";
+import PosterPlaceholder from "../asset/movie-placeholder.jpeg";
 
 const Nominations = ({ nominations }) => {
   const dispatch = useDispatch();
 
   return (
-    <>
+    <div>
+      <h1>NOMINATIONS</h1>
       {nominations && nominations.length > 0 && (
-        <div>
-          <h1>NOMINATIONS</h1>
-          <ul>
-            {nominations.map((nomination) => (
-              <li key={nomination.id}>
-                <p>{nomination.title}</p>
-                <p>{nomination.year}</p>
-                <button
-                  onClick={() => dispatch(deleteNomination(nomination.id))}
-                >
-                  X
-                </button>
-              </li>
-            ))}
-          </ul>
-          {nominations.length === 5 && <p>Your nomination list is ready!</p>}
-        </div>
+        <MovieGrid overflow={"hidden"}>
+          {nominations.map((nomination) => (
+            <MovieStyles key={nomination.id}>
+              <a
+                href={`https://www.imdb.com/title/${nomination.id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={nomination.poster !== "N/A" ? nomination.poster : PosterPlaceholder} alt={nomination.name} />
+              </a>
+              <p>
+                {nomination.title} ({nomination.year})
+              </p>
+              <button onClick={() => dispatch(deleteNomination(nomination.id))}>
+                X
+              </button>
+            </MovieStyles>
+          ))}
+        </MovieGrid>
       )}
-    </>
+      {nominations.length === 5 && <p>Your nomination list is ready!</p>}
+    </div>
   );
 };
 
