964bed423f6b5fa49667d8dcd9a249b77a3b14f2
peter <peterhanggg@gmail.com>
2021-01-15 14:37:04 -0500

    added env variables to react app

--- client/src/client.js
+++ client/src/client.js
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { HttpLink } from "apollo-link-http";
 
-const link = new HttpLink({ uri: "http://localhost:4000/" });
+const link = new HttpLink({
+  uri: `${process.env.REACT_APP_GRAPHQL_URI}`,
+});
 const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
